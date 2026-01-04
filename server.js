03078d818e8b701b6e0b6cde3511bb09c6faee13
Ashwin1122cseA <ashwin.2401021@srec.ac.in>
2026-01-04 12:45:25 +0530

    Railway connectivity test

--- server.js
+++ server.js
@@ -1,27 +1,11 @@
 const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const path = require("path");
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
 
-/* Serve static files */
-app.use(express.static(path.join(__dirname, "public")));
-
-/* IMPORTANT: Root route */
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "login.html"));
+  res.status(200).send("✅ Ashwin Remote is LIVE on Railway");
 });
 
-/* Socket.io */
-io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
-});
-
-/* Railway port */
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
